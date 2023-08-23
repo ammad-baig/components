e379a13f22c4c2722e4cfe9ffcc94638fe17426d
ammad-baig <ammadbaig50@gmail.com>
2023-08-23 19:03:51 +0500

    #updated

--- src/components/input.js
+++ src/components/input.js
@@ -4,7 +4,7 @@ const Input = (props) => {
   return (
     <div className={props.className}>
       <label>{props.label}</label>
-      <input  type={props.type } onChange={props.onChange} value={props.value} placeholder={props.placehrseaeolder} />
+      <input  type={props.type } onChange={props.onChange} value={props.value} placeholder={props.placeholder} />
     </div>
   );
 };
